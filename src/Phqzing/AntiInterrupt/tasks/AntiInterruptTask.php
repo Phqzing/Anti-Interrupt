4048c5bf4917fa933a321a04660fbc05d412c236
Phqzing <83136387+Phqzing@users.noreply.github.com>
2021-09-25 00:33:03 +0800

    Update AntiInterruptTask.php

--- src/Phqzing/AntiInterrupt/tasks/AntiInterruptTask.php
+++ src/Phqzing/AntiInterrupt/tasks/AntiInterruptTask.php
@@ -1,1 +1,31 @@
+<?php
 
+namespace Phqzing\AntiInterrupt\tasks;
+
+use pocketmine\scheduler\Task;
+use pocketmine\Player;
+use Phqzing\AntiInterrupt\AntiInterrupt;
+
+class AntiInterruptTask extends Task {
+  
+  
+  private $plugin;
+  
+  
+  public function __construct(AntiInterrupt $plugin){ 
+    $this->plugin = $plugin;
+  }
+  
+  
+  public function onRun(int $tick){
+    foreach($this->plugin->timer as $name => $time){
+      if($time === 0){
+        $player = $this->plugin->getServer()->getPlayerExact($name);
+        if($player instanceof Player){
+          $this->plugin->removeEnemy($player);
+        }
+      }
+      $this->plugin->timer[$name]--;
+    }
+  }
+}
